47c2684a876623ae51c1716ca69a4ec11eb61da9
Manojvittedi <153159811+Manojvittedi@users.noreply.github.com>
2026-04-06 18:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 function insert_Row() {
     //Write your code here
 	let table = document.getElementById("sampleTable");
+	// Insert row at the top (index 0)
+    const newRow = table.insertRow(0);
 
-	table.innerHTML = `
-		<tr><td>New Cell1</td> 
-		<td>New Cell2</td></tr>
-	` + table.innerHTML;
+    // Create cells
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    // Set values
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
+
+	// table.innerHTML = `
+	// 	<tr>
+	// 	<td>New Cell1</td> 
+	// 	<td>New Cell2</td>
+	// 	</tr>
+	// ` + table.innerHTML;
   
 }
